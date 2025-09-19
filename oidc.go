febfffee0c0803cf4eb86798709d35d46e2d23d8
yay101 <web@cdtech.au>
2025-09-19 15:01:58 +1000

    Fix: Use Providers type for providers argument

--- oidc.go
+++ oidc.go
@@ -14,7 +14,7 @@ import (
 	"time"
 )
 
-func NewClient(domains []string, providers []Provider, authpath string, loginpath string, logger *slog.Logger) *Client {
+func NewClient(domains []string, providers Providers, authpath string, loginpath string, logger *slog.Logger) *Client {
 	if logger == nil {
 		lj = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
 	}
